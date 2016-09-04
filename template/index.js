3bdaa7bab1bdd7466750e94990634673b45f97c3
Maximilian Fellner <max.fellner@gmail.com>
2016-09-04 16:08:33 +0200

    style: improve linting

--- template/index.js
+++ template/index.js
@@ -3,16 +3,16 @@ import Root from './components/Root'
 
 export default class Index extends Component {
   props: {
-    language: string,
-    title: string,
-    description: string
+    language: string;
+    title: string;
+    description: string;
   }
 
   render() {
     return <html lang={this.props.language}>
       <head>
-        <meta charset="utf-8"/>
-        <meta http-equiv="x-ua-compatible" content="ie=edge"/>
+        <meta charSet="utf-8"/>
+        <meta httpEquiv="x-ua-compatible" content="ie=edge"/>
         <title>{this.props.title}</title>
         <meta name="description" content={this.props.description}/>
         <meta name="viewport" content="width=device-width, initial-scale=1"/>
