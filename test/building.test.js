1c4a10d5b8a85385e7d1de1509e041e3e4d3ff1f
Maximilian Fellner <max.fellner@gmail.com>
2016-09-12 00:36:10 +0200

    feat: render blogposts as html

--- test/building.test.js
+++ test/building.test.js
@@ -2,11 +2,16 @@ import HelloComponent from './fixtures/hello-component.js'
 import { expectAsyncToThrow } from './helpers'
 
 describe('building', () => {
+  jest.mock('mz/fs')
   jest.mock('../lib/utils')
   jest.mock('../lib/file-utils')
+  jest.mock('../lib/render')
   jest.mock('../lib/project')
   jest.mock('../lib/Loader')
 
+  const fs = require('mz/fs')
+  const futils = require('../lib/file-utils')
+  const render = require('../lib/render')
   const project = require('../lib/project')
   const building = require('../lib/building')
   const HelloHTML = '<html><head><title>world</title></head><body></body></html>'
@@ -28,16 +33,22 @@ describe('building', () => {
       project.__setMockReturnValues({
         getProjectPath: '/hello'
       })
-      const futils = require('../lib/file-utils').__setMockReturnValues({
+      futils.__setMockReturnValues({
         readJSON: {
           template: 'world.js',
           title: 'world'
-        }
+        },
+        findFiles: (dir, exts ) => exts[0] === 'md' ? ['1970-01-01-hello.md'] : []
       })
+      render.__setMockReturnValues({
+        renderMarkdown: '<div>hello</div>'
+      })
+      fs.readFile = jest.fn(() => Promise.resolve(''))
 
       const result = await building.buildProject()
       expect(result).toBe('/hello/dist/index.html')
       expect(futils.writeFile).toBeCalledWith('/hello/dist/index.html', HelloHTML)
+      expect(futils.writeFile).toBeCalledWith('/hello/dist/posts/1970-01-01-hello.html', '<div>hello</div>')
     })
   })
 })
